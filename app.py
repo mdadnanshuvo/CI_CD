8b6d4738dfca3d0fd8d7d7ae6fd36f389172a146
Mohammad Adnan Shuvo <ahmedadnanshuvo@gmail.com>
2025-05-01 21:54:13 +0600

    # Feature   - Sample app to learn CI/CD.

---
+++ app.py
@@ -0,0 +1,11 @@
+class MyApp:
+    def Hello_CICD(self):
+        print("Hello, CI/CD!")
+
+    def main(self):
+        self.Hello_CICD()
+
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.main()
